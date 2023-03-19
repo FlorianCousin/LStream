220a933f473dec00cfb1a6807728c4e4d99a03fb
FlorianCousin <florian.cousin@polyconseil.fr>
2023-03-19 23:59:14 +0100

    Reduce

--- src/main/java/florian/cousin/LinearStream.java
+++ src/main/java/florian/cousin/LinearStream.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -30,5 +31,13 @@ public interface LinearStream<T> extends Iterator<T> {
     List<T> collectingList = new ArrayList<>();
     forEachRemaining(collectingList::add);
     return Collections.unmodifiableList(collectingList);
+  }
+
+  default <R> R reduce(R initialValue, BiFunction<R, T, R> aggregate) {
+    R currentValue = initialValue;
+    while (hasNext()) {
+      currentValue = aggregate.apply(currentValue, next());
+    }
+    return currentValue;
   }
 }
