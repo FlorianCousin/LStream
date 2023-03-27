ec87126cabb16a8532132a3ff325742c3a3c14ea
FlorianCousin <florian.cousin@polyconseil.fr>
2023-03-27 22:27:48 +0200

    Import only what needed

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module LinearStream {
+
+    requires lombok;
+    requires org.jetbrains.annotations;
+
+    exports florian.cousin;
+    exports florian.cousin.iterator;
+    exports florian.cousin.collector;
+}
