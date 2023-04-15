bc85f5d38c3c87dc2ce2e15bbcf313002b716f48
FlorianCousin <florian.cousin@polyconseil.fr>
2023-04-15 17:30:00 +0200

    Test null values with map

--- src/test/java/florian/cousin/LinearStreamNullValuesTest.java
+++ src/test/java/florian/cousin/LinearStreamNullValuesTest.java
@@ -1,9 +1,11 @@
 package florian.cousin;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class LinearStreamNullValuesTest {
@@ -13,6 +15,24 @@ class LinearStreamNullValuesTest {
 
     List<Integer> actualNullValues = LinearStream.of(1, null, 6).filter(Objects::isNull).toList();
 
-    Assertions.assertThat(actualNullValues).containsExactlyElementsOf(Collections.singleton(null));
+    assertThat(actualNullValues).containsExactlyElementsOf(Collections.singleton(null));
+  }
+
+  @Test
+  void map() {
+
+    List<Integer> actualValues =
+        LinearStream.of(null, 2).map(number -> number == null ? 3 : null).toList();
+
+    List<Integer> expectedValues = buildArrayList(3, null);
+
+    assertThat(actualValues).isEqualTo(expectedValues);
+  }
+
+  @SafeVarargs
+  private <T> List<T> buildArrayList(T... values) {
+    ArrayList<T> list = new ArrayList<>();
+    Collections.addAll(list, values);
+    return list;
   }
 }
