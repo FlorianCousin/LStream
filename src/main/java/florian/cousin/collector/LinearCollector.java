1b916e4d16a1242aabebef367572f819d2543708
FlorianCousin <florian.cousin@polyconseil.fr>
2023-10-03 21:46:06 +0200

    Give an improvement idea

--- src/main/java/florian/cousin/collector/LinearCollector.java
+++ src/main/java/florian/cousin/collector/LinearCollector.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 public class LinearCollector<T, A, R> {
 
   private final Supplier<A> supplier;
+  // TODO Use BiFunction<A, ? super T, A> ?
   private final BiConsumer<A, ? super T> accumulator;
   private final Function<A, R> finisher;
 
