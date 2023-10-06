663ea36203d4e34acd0f9874475c08eeac3f4ab3
FlorianCousin <florian.cousin@polyconseil.fr>
2023-10-06 18:54:42 +0200

    Improve measurement

--- src/test/java/florian/cousin/benchmarks/DifferentiateBigFilterMapBenchmark.java
+++ src/test/java/florian/cousin/benchmarks/DifferentiateBigFilterMapBenchmark.java
@@ -3,6 +3,7 @@ package florian.cousin.benchmarks;
 import florian.cousin.LStream;
 import java.math.BigInteger;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -10,17 +11,17 @@ import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
 /*
- * Benchmark                          Mode  Cnt    Score    Error  Units
- * DifferentiateBigFilterMapBenchmark.lstreamGenerate  avgt   10   99.931 ±  2.314  ms/op
- * DifferentiateBigFilterMapBenchmark.lstreamFilter    avgt   10  138.848 ±  5.723  ms/op
- * DifferentiateBigFilterMapBenchmark.lstreamMap       avgt   10  151.599 ±  4.873  ms/op
- * DifferentiateBigFilterMapBenchmark.lstreamToList    avgt   10  245.338 ± 14.463  ms/op
+ * Benchmark                                           Mode  Cnt    Score   Error  Units
+ * DifferentiateBigFilterMapBenchmark.lstreamGenerate  avgt   10   43.197 ± 0.771  ms/op
+ * DifferentiateBigFilterMapBenchmark.lstreamFilter    avgt   10  103.327 ± 0.971  ms/op
+ * DifferentiateBigFilterMapBenchmark.lstreamMap       avgt   10  114.149 ± 1.750  ms/op
+ * DifferentiateBigFilterMapBenchmark.lstreamToList    avgt   10  177.028 ± 9.231  ms/op
  */
 
 @State(Scope.Benchmark)
 @Fork(value = 2, warmups = 1)
-@Warmup(iterations = 10, time = 1)
-@Measurement(iterations = 5, time = 1)
+@Warmup(iterations = 10, time = 2)
+@Measurement(iterations = 5, time = 2)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class DifferentiateBigFilterMapBenchmark {
@@ -28,40 +29,39 @@ public class DifferentiateBigFilterMapBenchmark {
   private static final int NB_ELEMENTS = BigInteger.TEN.pow(7).intValueExact();
   private static final Random RANDOM = new Random(Instant.now().getNano());
 
+  private static List<Integer> testData;
+
+  @Setup(Level.Iteration)
+  public void setUp() {
+    testData = new ArrayList<>(NB_ELEMENTS);
+    for (int i = 0; i < NB_ELEMENTS; i++) {
+      testData.add(RANDOM.nextInt(-50, 51));
+    }
+  }
+
   @Benchmark
   public void lstreamGenerate(Blackhole blackhole) {
 
-    LStream.generate(() -> RANDOM.nextInt(-50, 51)).limit(NB_ELEMENTS).forEach(blackhole::consume);
+    LStream.from(testData).limit(NB_ELEMENTS).forEach(blackhole::consume);
   }
 
   @Benchmark
   public void lstreamFilter(Blackhole blackhole) {
 
-    LStream.generate(() -> RANDOM.nextInt(-50, 51))
-        .limit(NB_ELEMENTS)
-        .filter(i -> i > 0)
-        .forEach(blackhole::consume);
+    LStream.from(testData).filter(i -> i > 0).forEach(blackhole::consume);
   }
 
   @Benchmark
   public void lstreamMap(Blackhole blackhole) {
 
-    LStream.generate(() -> RANDOM.nextInt(-50, 51))
-        .limit(NB_ELEMENTS)
-        .filter(i -> i > 0)
-        .map(i -> i - 1)
-        .forEach(blackhole::consume);
+    LStream.from(testData).filter(i -> i > 0).map(i -> i - 1).forEach(blackhole::consume);
   }
 
   @Benchmark
   public void lstreamToList(Blackhole blackhole) {
 
     List<Integer> generatedList =
-        LStream.generate(() -> RANDOM.nextInt(-50, 51))
-            .limit(NB_ELEMENTS)
-            .filter(i -> i > 0)
-            .map(i -> i - 1)
-            .toList();
+        LStream.from(testData).filter(i -> i > 0).map(i -> i - 1).toList();
 
     blackhole.consume(generatedList);
   }
