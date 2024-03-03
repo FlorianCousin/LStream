144233bb194f678f41f3273231f0f0a87edf8251
FlorianCousin <florian.cousin@polyconseil.fr>
2024-03-03 15:24:49 +0100

    Add a task

--- src/main/java/florian/cousin/LStream.java
+++ src/main/java/florian/cousin/LStream.java
@@ -17,6 +17,9 @@ public abstract class LStream<T> implements Iterator<T>, LStreamApi<T> {
   //  We could do list.stream.withIndexes().filter(object -> filter(object)).indexes() and it would
   //  make an IntStream of the indexes filtered
 
+  // TODO Check why it is not possible to return MappingLStream etc... instead of LStream, and make
+  //  a ADR
+
   @Override
   public LStream<T> filter(Predicate<? super T> predicate) {
     return new FilterLStream<>(this, predicate);
