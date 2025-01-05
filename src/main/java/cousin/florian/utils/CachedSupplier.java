679ff26bbc12bfd77fa8e12451d0612250ea5322
FlorianCousin <florian.cousin@polyconseil.fr>
2025-01-05 12:56:10 +0100

    Check format

--- src/main/java/cousin/florian/utils/CachedSupplier.java
+++ src/main/java/cousin/florian/utils/CachedSupplier.java
@@ -1,25 +1,24 @@
 package cousin.florian.utils;
 
-import lombok.RequiredArgsConstructor;
-
 import java.util.function.Supplier;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class CachedSupplier<Element> implements Supplier<Element> {
 
-    private boolean cacheIsInitialised = false;
-    private Element cachedValue;
-    private final Supplier<Element> supplierWithoutCache;
+  private boolean cacheIsInitialised = false;
+  private Element cachedValue;
+  private final Supplier<Element> supplierWithoutCache;
 
-    @Override
-    public Element get() {
+  @Override
+  public Element get() {
 
-        if (cacheIsInitialised) {
-            return cachedValue;
-        }
-
-        cachedValue = supplierWithoutCache.get();
-        cacheIsInitialised = true;
-        return cachedValue;
+    if (cacheIsInitialised) {
+      return cachedValue;
     }
+
+    cachedValue = supplierWithoutCache.get();
+    cacheIsInitialised = true;
+    return cachedValue;
+  }
 }
